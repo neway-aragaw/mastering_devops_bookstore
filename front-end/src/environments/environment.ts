d01efaf1624e9b564a86467b52c7b5260f2b5f37
Neway Aragaw <niwaygebrie.com>
2025-02-05 20:52:23 -0500

    Added shopping cart UI

---
+++ front-end/src/environments/environment.ts
@@ -0,0 +1,12 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `angular-cli.json`.
+
+export const environment = {
+  production: false,
+  resourceApi: "http://18.117.82.204/resources-api",
+  inventoryApi: "http://18.117.82.204/inventory-api",
+  clientApi: "http://18.117.82.204/clients-api",
+  rentingApi: "http://18.117.82.204/renting-api"
+};
